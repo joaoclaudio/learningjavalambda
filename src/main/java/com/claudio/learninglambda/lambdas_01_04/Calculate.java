4153b299c8deb1fd173fb474506aed496e3a5e46
Cláudio Venâncio <jclaudio.7@gmail.com>
2019-02-24 17:25:56 -0300

    Finaliza unidade 01_04

---
+++ src/main/java/com/claudio/learninglambda/lambdas_01_04/Calculate.java
@@ -0,0 +1,9 @@
+package com.claudio.learninglambda.lambdas_01_04;
+
+/**
+ * Calculate
+ */
+@FunctionalInterface
+public interface Calculate {
+    int calc(int x, int y);    
+}
---
+++ src/main/java/com/claudio/learninglambda/lambdas_01_04/lambdas_01_04.java
@@ -0,0 +1,23 @@
+package com.claudio.learninglambda.lambdas_01_04;
+
+/**
+ * lambdas_01_04
+ */
+public class lambdas_01_04 {
+
+    public static void main(String[] args) {
+        
+        Calculate add = (a, b) -> a+b;
+        Calculate difference = (a, b) -> a-b;
+        Calculate divide = (a, b) -> (b != 0 ? a / b : 0);
+        Calculate multiply = (e, f) -> e*f;
+
+        System.out.println(add.calc(10, 15));
+        System.out.println(difference.calc(20, 3));
+        System.out.println(divide.calc(40, 3));
+        System.out.println(divide.calc(27, 0));
+        System.out.println(multiply.calc(4, 33));
+
+
+    }
+}
