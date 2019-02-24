eaabd45fe8a7571c66363c238097c31500c860cb
Cláudio Venâncio <jclaudio.7@gmail.com>
2019-02-24 12:25:05 -0300

    Exemplos lambda java

--- src/main/java/com/claudio/learninglambda/App.java
+++ src/main/java/com/claudio/learninglambda/App.java
@@ -1,5 +1,7 @@
 package com.claudio.learninglambda;
 
+import java.util.function.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Teste");   
+        //Using the test method of Predicate
+        Predicate<String> stringLen = (s) -> s.length() < 10;
+        System.out.println(stringLen.test("Apples")+" - Apples is less than 10");
+
+        //Consumer example uses accept method
+        Consumer<String> consumerStr = (s) -> System.out.println(s.toLowerCase());
+        consumerStr.accept("ABCerADREasdfasdfasdrfaser");
+
+        //Function example
+        Function<Integer, String> converter = (num) -> Integer.toString(num);
+        System.out.println("lengthof 26: " + converter.apply(26).length());
+
+        //Supplier example
+        Supplier<String> s = () -> "Java is fun";
+        System.out.println(s.get());
+
+        //Binary Operator example
+        BinaryOperator<Integer> add = (a, b) -> a + b;
+        System.out.println("add 10 + 25: "+add.apply(10, 25));
+
+        //Unary operator example
+        UnaryOperator<String> str = (msg) -> msg.toUpperCase();
+        System.out.println(str.apply("This is my message in upper case"));
     }
 }
